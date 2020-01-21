e4549c0f289bb824c349da7aec1f7cef5a343eeb
Igo Jeferson Barros Coelho <igojeferson.coelho@gmail.com>
2020-01-21 17:08:28 -0300

    - Included gradle dependencies for correct springboot project structuring - Add eureka server configuration

--- eureka-server/src/main/java/com/mercadao/eurekaserver/controller/HelloGradleController.java
+++ eureka-server/src/main/java/com/mercadao/eurekaserver/controller/HelloGradleController.java
@@ -3,12 +3,12 @@ package com.mercadao.eurekaserver.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController("/health")
 public class HelloGradleController {
 
     @GetMapping
     public String helloGradle() {
-        return "Hello Gradle!";
+        return "200";
     }
 
 }
