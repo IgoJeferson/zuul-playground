20a971af0e344f208b437acaab4045dffe4f1bf9
Igo Jeferson Barros Coelho <igojeferson.coelho@gmail.com>
2020-01-24 16:53:19 -0300

    - Add simple logger filter to log all requests ( just method and url )

--- api-gateway/src/main/java/com/mercadao/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mercadao/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.mercadao.apigateway;
 
+import com.mercadao.apigateway.filter.LoggerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,6 +14,11 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public LoggerFilter loggerFilter(){
+        return new LoggerFilter();
     }
 
 }
